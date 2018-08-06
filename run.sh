6296c7c19528c10cc5b2da109a88afb604a37900
lc4t <lc4t0.0@gmail.com>
2018-08-06 16:53:41 +0800

    add

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 nohup python crawler_booter.py --usage crawler common ajax > crawler.log 2>&1 &
 nohup python scheduler_booter.py --usage crawler common ajax > crawler_scheduler.log 2>&1 &
-nohup python crawler_booter.py --usage validator init > init_validator.log 2>&1 &
-nohup python crawler_booter.py --usage validator bili > bili_validator.log 2>&1&
+nohup python crawler_booter.py --usage validator init bili > init_validator.log 2>&1 &
 nohup python scheduler_booter.py --usage validator bili > validator_scheduler.log 2>&1 &
 #nohup python squid_update.py --usage bili --interval 3 > squid.log 2>&1 &
 #rm -rf /var/run/squid.pid
